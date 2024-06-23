9c94bee1833549ac2b131e73430df4059e89a1a6
Jorerock <jorerock@laposte.net>
2024-06-23 22:24:33 +0200

    ORM Eloquent

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // permet d'etablir les champs remplissable de la BDD
+    protected $fillable = [
+        'title',
+        'slug',
+        'content'
+    ];
+
+    // permet d'etablir les champs NON remplissable de la BDD
+    // protected $guarded = [];
 }
--- routes/web.php
+++ routes/web.php
@@ -41,53 +41,72 @@ route::prefix('/blog')->name('blog.')->group(function(){
 
 
 
-Route::get('/', function(Request $request){
+    Route::get('/', function(Request $request){
 
-    // $post = new \App\Models\Post();
-    // $post->title = 'mon premier article 2';
-    // $post->slug = 'mon premier article 2';
-    // $post->content = 'mon contenue 2';
-    // $post->save();
-    //   return $post;
 
-    // $posts = \App\Models\Post::all(['id', 'title']);
+        // return $post;
+        // $posts = \App\Models\Post::all(['id', 'title']);
         // $posts = \App\Models\Post::findOrFail(2);
-   return \App\Models\Post::paginate(1,['id','title']);
+        //   return \App\Models\Post::paginate(1,['id','title']);
+        // $posts = \App\Models\Post::where('id','>',0)->get();
+        // dd($posts);
+
+        $return  = \App\Models\Post::paginate(25);
+
+        // return [
+        //     "name"=>$_GET['name'],
+        //     //$_GET['name'] renvoie le paramettre mis dans l'url de la page mais error s'il n'y as pas
+        //     "article"=> "article 1"
+        // ];
+    })->name('Main');
 
 
-    // dd($posts);
+    Route::get('/delete', function(Request $request){
+        $posts = \App\Models\Post::find(1);
+        $posts->delete();
+    })->name('Delete');
 
-    return [
-        "name"=>$_GET['name'],
-        //$_GET['name'] renvoie le paramettre mis dans l'url de la page mais error s'il n'y as pas
-        "article"=> "article 1"
-    ];
-});
+    Route::get('/add', function(Request $request){
+        $post = new \App\Models\Post();
+        $post->title = 'mon deuxieme article';
+        $post->slug = 'mon deuxieme article';
+        $post->content = 'mon deuxieme contenue ';
+        $post->save();
+    })->name('add');
 
-
-Route::get('/input', function(Request $request){
-    return  $request->input('name',"john doe");
-    //input renvoie le parametre attribué, mais ne renvoie pas d'erreur en cas d'absence, sont deuxieme param est l'elem par default
-})->name('index');
-
-
-
-Route::get('/{slug}-{id}', function (string $slug, string $id ,request $request){
-    return [
-        "slug"=>$slug,
-        "id"=> $id,
-        "name"=> $request->input(('name')),
-        "link"=>\route('blog.show',['slug'=>'articles','id'=>'13'])
-    ];
-  //  http://127.0.0.1:8000/blog/mon-premier-articles-12
+    Route::get('/add-grouped', function(Request $request){
+        $posts = \App\Models\Post::create([
+            'title'=>'mon deuxieme Nouveau titre',
+            'slug' => 'Deuxieme-titre-test',
+            'content' => 'contenue',
+        ]);
+        return $posts;
+    })->name('add-group');
 
 
-})->where([
-    'id' => '[0-9]+',
-    'slug' => '[a-z0-9\-]+'
-])->name('show');
-    //http://127.0.0.1:8000/blog/mon-premier-articles-12
-    //utilise des expression réguliere pour ajouté des contrainte a la route
+    Route::get('/input', function(Request $request){
+        return  $request->input('name',"john doe");
+        //input renvoie le parametre attribué, mais ne renvoie pas d'erreur en cas d'absence, sont deuxieme param est l'elem par default
+    })->name('index');
+
+
+
+    Route::get('/{slug}-{id}', function (string $slug, string $id ,request $request){
+        return [
+            "slug"=>$slug,
+            "id"=> $id,
+            "name"=> $request->input(('name')),
+            "link"=>\route('blog.show',['slug'=>'articles','id'=>'13'])
+        ];
+    //  http://127.0.0.1:8000/blog/mon-premier-articles-12
+
+
+    })->where([
+        'id' => '[0-9]+',
+        'slug' => '[a-z0-9\-]+'
+    ])->name('show');
+        //http://127.0.0.1:8000/blog/mon-premier-articles-12
+        //utilise des expression réguliere pour ajouté des contrainte a la route
 
 
 
